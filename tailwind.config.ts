452120d767baf6d27e0ca02bfa83d5d40c20023e
voiceoverguy-uk <42624173-voiceoverguy-uk@users.noreply.replit.com>
2025-09-29 16:51:55 +0000

    Restored to 'c27fe6c73c219cb5635ffa9792c009ee35c43b3b'
    Replit-Restored-To: c27fe6c73c219cb5635ffa9792c009ee35c43b3b

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,9 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./client/index.html",
+              "./client/src/**/*.{js,ts,jsx,tsx}",
+              "./server/**/*.{ts,tsx}"],
   theme: {
     extend: {
       borderRadius: {
